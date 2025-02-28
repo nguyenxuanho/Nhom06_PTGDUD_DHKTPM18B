9202ad5fd3663ffd789e7813acfc2389f0301095
Xuanho1710 <xuanhodcbas@gmail.com>
2025-02-28 19:54:01 +0700

    Học TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className='md:transition-all lg:transition-all sm:transition-all xl:transition-all transition-all container grid xl:gap-3 md:gap-3 sm:gap-3 gap-1 grid-flow-row grid-cols-2 xl:grid-cols-5 sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-3'>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-cyan-600 col-span-2 flex justify-center h-72 w-auto items-center'>Chiếm 2 cột</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-purple-600 row-span-2 flex justify-center h-auto w-auto items-center'>Chiếm 2 dòng</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+          <div className='bg-green-600 flex justify-center h-72 w-auto items-center'>Box</div>
+        </div>
+      </>
   );
 }
 
